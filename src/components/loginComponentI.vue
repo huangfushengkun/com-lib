042dc7cbb9c494db5f8264b2a19277a6c0698d5a
huangfushengkun <huangfushengkun@163.com>
2020-06-03 17:30:39 +0800

    注释使用图片的代码

--- src/components/loginComponentI.vue
+++ src/components/loginComponentI.vue
@@ -1,165 +1,158 @@
 <template>
   <section class="login-item-container">
-     <h1>{{conf.title}}</h1>
-     <div class="login-item-content">
-     <img src="../assets/images/logIn_bg.087b95b.png" width="100%" />    
-     <el-form :model="source" ref="loginForm" :rules="conf.rules">
-
-         <el-form-item prop="account">
-             <img src="../assets/images/user.png"/>
-             <el-select
-               :placeholder="conf.placeholder[0]"
-               v-model="source.account"
-             >
-                 <el-option
-                  v-for="(item,key) in conf.role"
-                  :key="key"
-                  :label="item.name"
-                  :value="item.value"
-                 ></el-option>
-             </el-select>
-         </el-form-item>
-         <el-form-item prop="checkPass">
-             <img src="../assets/images/pwd.png"/>
-             <el-input 
-               type="password"
-               auto-complete="off"
-               :placeholder="conf.placeholder[1]"
-               v-model="source.checkPass"
-            >
-              </el-input>
-         </el-form-item>
-         <el-button 
-            type="primary"
-            :loading="loading"
-            @click="fLoginBtnClick"
-        >登录</el-button>
-     </el-form>
-     </div>
-  </section>        
+    <h1>{{ conf.title }}</h1>
+    <div class="login-item-content">
+      <!-- <img src="../assets/images/logIn_bg.087b95b.png" width="100%" />     -->
+      <el-form :model="source" ref="loginForm" :rules="conf.rules">
+        <el-form-item prop="account">
+          <!-- <img src="../assets/images/user.png"/> -->
+          <el-select
+            :placeholder="conf.placeholder[0]"
+            v-model="source.account"
+          >
+            <el-option
+              v-for="(item, key) in conf.role"
+              :key="key"
+              :label="item.name"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item prop="checkPass">
+          <!-- <img src="../assets/images/pwd.png"/> -->
+          <el-input
+            type="password"
+            auto-complete="off"
+            :placeholder="conf.placeholder[1]"
+            v-model="source.checkPass"
+          >
+          </el-input>
+        </el-form-item>
+        <el-button type="primary" :loading="loading" @click="fLoginBtnClick"
+          >登录</el-button
+        >
+      </el-form>
+    </div>
+  </section>
 </template>
 <script>
 export default {
-    name:"loginComponentI",
-    data(){
-        return {
-            loading:false
+  name: "loginComponentI",
+  data() {
+    return {
+      loading: false
+    };
+  },
+  props: ["conf", "source", "loginBtnClick"],
+  methods: {
+    fLoginBtnClick: function() {
+      this.$refs["loginForm"].validate(valid => {
+        if (valid) {
+          this.loading = true;
+          this.loginBtnClick();
         }
-    },
-    props:[
-        "conf",
-        "source",
-        "loginBtnClick"
-    ],
-    methods:{
-       fLoginBtnClick:function(){
-         this.$refs['loginForm'].validate((valid)=>{
-             if(valid){
-                 this.loading=true;
-                 this.loginBtnClick();
-             }
-         })
-       }
+      });
     }
-}
+  }
+};
 </script>
 <style lang="scss">
-@include themems(){
-.login-item-container{
-    .el-input__inner{
-        background-color:transparent;
-        border:none;
-        height: 38px;
-        line-height: 38px;
-        font-size:14px;
-        color: getTheme('input_color');
+@include themems() {
+  .login-item-container {
+    .el-input__inner {
+      background-color: transparent;
+      border: none;
+      height: 38px;
+      line-height: 38px;
+      font-size: 14px;
+      color: getTheme("input_color");
     }
-    .el-input{
-        background-color: getTheme('input_bg_color');
-        color:  getTheme('input_color');
-        width: 350px;
-        border: 1px solid getTheme('input_border_color');
-        border-color: getTheme('input_border_color');
-        padding-left:32px;
-        box-sizing: border-box;
-        border-radius: 4px;
+    .el-input {
+      background-color: getTheme("input_bg_color");
+      color: getTheme("input_color");
+      width: 350px;
+      border: 1px solid getTheme("input_border_color");
+      border-color: getTheme("input_border_color");
+      padding-left: 32px;
+      box-sizing: border-box;
+      border-radius: 4px;
     }
+  }
+  .el-select-dropdown__item.hover,
+  .el-select-dropdown__item:hover {
+    background-color: rgba(1, 143, 252, 0.3);
+  }
+  .el-select-dropdown__item.selected {
+    color: #409eff;
+    font-weight: 700;
+  }
+  .el-select-dropdown__item {
+    color: getTheme("select_color");
+  }
+  .el-select-dropdown {
+    position: absolute;
+    border: 1px solid getTheme("select_border_color");
+    border-radius: 4px;
+    background-color: getTheme("select_bg_color");
+    color: getTheme("select_color");
+  }
+  .el-popper[x-placement^="bottom"] .popper__arrow::after {
+    border-bottom-color: getTheme("select_bg_color");
+  }
+  .el-popper[x-placement^="bottom"] .popper__arrow {
+    border-bottom-color: getTheme("select_border_color");
+  }
 }
- .el-select-dropdown__item.hover, .el-select-dropdown__item:hover {
-      background-color: rgba(1, 143, 252, 0.3);
-    }
-    .el-select-dropdown__item.selected {
-      color: #409EFF;
-      font-weight: 700;
-    }
-    .el-select-dropdown__item {
-      color: getTheme('select_color');
-    }
-    .el-select-dropdown {
-        position: absolute;
-        border: 1px solid getTheme('select_border_color');
-        border-radius: 4px;
-        background-color: getTheme('select_bg_color');
-        color: getTheme('select_color');
-    }
-    .el-popper[x-placement^=bottom] .popper__arrow::after{
-        border-bottom-color:getTheme('select_bg_color');
-    }
-    .el-popper[x-placement^=bottom] .popper__arrow{
-        border-bottom-color:getTheme('select_border_color');
-    }
-}
-
 </style>
 <style lang="scss" scoped>
-@include themems(){
-  .login-item-container{
-     h1{
-         font-size: 36px;
-         font-weight: 600;
-         color:getTheme('title_color');
-         letter-spacing: .5rem;
-         text-align: center;
-         margin-bottom: 30px;
-     }
-     .login-item-content{
-         position: relative;
-     }
-     .el-form{
-         width: 350px;
-         position: absolute;
-         margin: auto;
-         top:0px;
-         left: 0px;
-         bottom:0px;
-         right: 0px;
-     }
-     .el-form-item{
-         margin-top:40px;
-     }
-     .el-form-item__content{
-        img{
-            position: absolute;
-            z-index: 2;
-            left: 16px;
-            top:0px;
-            bottom:0px;
-            margin-top: auto;
-            margin-bottom:auto; 
-        }
-     }
-     .el-select{
-         width: 100%;
-     }
-     .el-button--primary{
-        background: getTheme('btn_bg_color');
-        color: getTheme('btn_color');
-        border: 1px solid getTheme('btn_border_color');
-        width: 100%;
-        letter-spacing:.5rem;
-        padding:12px 0px;
-        margin-top:20px; 
-     }
+@include themems() {
+  .login-item-container {
+    h1 {
+      font-size: 36px;
+      font-weight: 600;
+      color: getTheme("title_color");
+      letter-spacing: 0.5rem;
+      text-align: center;
+      margin-bottom: 30px;
+    }
+    .login-item-content {
+      position: relative;
+    }
+    .el-form {
+      width: 350px;
+      position: absolute;
+      margin: auto;
+      top: 0px;
+      left: 0px;
+      bottom: 0px;
+      right: 0px;
+    }
+    .el-form-item {
+      margin-top: 40px;
+    }
+    .el-form-item__content {
+      img {
+        position: absolute;
+        z-index: 2;
+        left: 16px;
+        top: 0px;
+        bottom: 0px;
+        margin-top: auto;
+        margin-bottom: auto;
+      }
+    }
+    .el-select {
+      width: 100%;
+    }
+    .el-button--primary {
+      background: getTheme("btn_bg_color");
+      color: getTheme("btn_color");
+      border: 1px solid getTheme("btn_border_color");
+      width: 100%;
+      letter-spacing: 0.5rem;
+      padding: 12px 0px;
+      margin-top: 20px;
+    }
   }
 }
 </style>
--- src/layouts/layoutI.vue
+++ src/layouts/layoutI.vue
@@ -1,113 +1,119 @@
 <template>
-    <section class="container">
-        <header-component-i
-          class="header-wrap"
-          :title="title"
-          :logo="logo"
-          :checkoutUser="fCheckoutUser"
-          :logout="fLoginOut"
-          :accountInfos="accountInfos"
-        />
-        <el-row tag="main" class="main-wrap">
-            <el-col :span="3" tag="aside" class="menu-wrap">
-               <menu-component-i />
-            </el-col>
-            <el-col :span="21" class="main-container">
-               <router-view></router-view>
-            </el-col>
-        </el-row>
-    </section>
+  <section class="container">
+    <header-component-i
+      class="header-wrap"
+      :title="title"
+      :logo="logo"
+      :checkoutUser="fCheckoutUser"
+      :logout="fLoginOut"
+      :accountInfos="accountInfos"
+    />
+    <el-row tag="main" class="main-wrap">
+      <el-col :span="3" tag="aside" class="menu-wrap">
+        <menu-component-i />
+      </el-col>
+      <el-col :span="21" class="main-container">
+        <router-view></router-view>
+      </el-col>
+    </el-row>
+  </section>
 </template>
 <script>
-import {menuComponentI,headerComponentI} from '@/index'
+import { menuComponentI, headerComponentI } from "@/index";
 export default {
-    name:"layout-I",
-    data(){
-        return{
-            title:"运行管理系统",
-            logo:require("../assets/images/logo.png"),
-            accountInfos:{
-                title:{
-                    name:"管理员",
-                    icon:"el-icon-user-solid"
-                },
-                list:[{
-                    name:"切换账号",
-                    icon:"el-icon-user",
-                    command:"checkoutUser"
-                },{
-                    name:"退出",
-                    icon:"el-icon-delete",
-                    command:"logout"
-                }]
-            }
-        }
-    },
-    components:{
-        headerComponentI,
-        menuComponentI
-    },
-    methods:{
-        /*
-         * 切换用户
-         * */
-        fCheckoutUser(){
-            this.$confirm("确认注销吗？","提示",{
-                confirmButtonText:"确定",
-                cancelButtonText:'取消',
-                showClose: false
-            }).then(()=>{
-                this.$router.push('/login');
-            }).catch(()=>{
-                console.log('取消操作');
-            })
+  name: "layout-I",
+  data() {
+    return {
+      title: "运行管理系统",
+      // logo:require("../assets/images/logo.png"),
+      accountInfos: {
+        title: {
+          name: "管理员",
+          icon: "el-icon-user-solid"
         },
-        /*
-         * 退出登录
-         * */
-        fLoginOut(){
-            this.$confirm("确认退出吗？","提示",{
-                confirmButtonText:"确定",
-                cancelButtonText:'取消',
-                showClose: false
-            }).then(()=>{
-
-            }).catch(()=>{
-                console.log('取消操作');
-            })
-        }
+        list: [
+          {
+            name: "切换账号",
+            icon: "el-icon-user",
+            command: "checkoutUser"
+          },
+          {
+            name: "退出",
+            icon: "el-icon-delete",
+            command: "logout"
+          }
+        ]
+      }
+    };
+  },
+  components: {
+    headerComponentI,
+    menuComponentI
+  },
+  methods: {
+    /*
+     * 切换用户
+     * */
+    fCheckoutUser() {
+      this.$confirm("确认注销吗？", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        showClose: false
+      })
+        .then(() => {
+          this.$router.push("/login");
+        })
+        .catch(() => {
+          console.log("取消操作");
+        });
+    },
+    /*
+     * 退出登录
+     * */
+    fLoginOut() {
+      this.$confirm("确认退出吗？", "提示", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        showClose: false
+      })
+        .then(() => {})
+        .catch(() => {
+          console.log("取消操作");
+        });
     }
-}
+  }
+};
 </script>
 <style lang="scss" scoped>
- .container{
-     height: 100%;
-     background: url("../../static/images/bg.7969588.png") center center no-repeat transparent scroll;
-     background-size: cover;
- }
-.main-wrap{
-     height: calc(100% - 58px);
-     height: -webkit-calc(100% - 58px);
-     height: -moz-calc(100% - 58px);
- }
- /*滚动条的宽度*/
-::-webkit-scrollbar {
-	width:1px;
+.container {
+  height: 100%;
+  background-color: #0c0c79;
+  //  background: url("../../static/images/bg.7969588.png") center center no-repeat transparent scroll;
+  //   background-size: cover;
 }
- .header-wrap{
-     height: 58px;
-     line-height: 58px;
-     margin: 0px 18px;
- }
- .menu-wrap{
-     overflow-y: auto;
-     overflow-x: hidden;
-     height: 100%;
-     -webkit-box-shadow: 0 0 12px rgba(0, 127, 255, 0.24) inset;
-     box-shadow: 0 0 12px rgba(0, 127, 255, 0.24) inset;
- }
-.main-container{
-    height: 100%;
-    overflow: auto;
+.main-wrap {
+  height: calc(100% - 58px);
+  height: -webkit-calc(100% - 58px);
+  height: -moz-calc(100% - 58px);
+}
+/*滚动条的宽度*/
+::-webkit-scrollbar {
+  width: 1px;
+}
+.header-wrap {
+  height: 58px;
+  line-height: 58px;
+  margin: 0px 18px;
+}
+.menu-wrap {
+  overflow-y: auto;
+  overflow-x: hidden;
+  height: 100%;
+  -webkit-box-shadow: 0 0 12px rgba(0, 127, 255, 0.24) inset;
+  box-shadow: 0 0 12px rgba(0, 127, 255, 0.24) inset;
+}
+.main-container {
+  height: 100%;
+  overflow: auto;
 }
 </style>
